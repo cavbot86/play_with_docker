9f10a5f8e7eb537c2a3ff6307c6487c09283a361
Cavbot <cavbot@outlook.com>
2019-10-24 22:43:41 +0800

    docker_run_test: create

---
+++ docker_run_test.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+IP_192=`cmd-get-ip-192`
+PORT_1=58822
+
+if [ -n "$IP_192" ]; then
+    PORT_1=$IP_192:$PORT_1
+fi
+
+rm -rf ${DOCKER_VOLUMES}/docker_temp_test
+mkdir -p ${DOCKER_VOLUMES}/docker_temp_test
+
+sudo docker run -d \
+    -p $PORT_1:22 \
+    --name docker_temp_test \
+    cavbot/temp_for_test
