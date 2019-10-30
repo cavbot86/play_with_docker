1348ee46ba88a918bfc11fd099bbc252fda429e2
hizhangrui <hizhangrui@hotmail.com>
2019-10-30 18:42:42 +0800

    docker_run_test:

--- docker_run_test.sh
+++ docker_run_test.sh
@@ -6,11 +6,14 @@ if [ -n "$IP_192" ]; then
     PORT_1=$IP_192:$PORT_1
 fi
 
-# rm -rf ${DOCKER_VOLUMES}/docker_temp_test
-mkdir -p ${DOCKER_VOLUMES}/docker_temp_test
+
+sudo mkdir -p ${DOCKER_VOLUMES}/docker_temp_test/develop/data
+sudo mkdir -p ${DOCKER_VOLUMES}/ssh_id_rsa
 
 sudo docker run -it \
     -p $PORT_1:22 \
+    -v ${DOCKER_VOLUMES}/docker_temp_test/ubuntu-base-flat/data:/data \
+    -v ${DOCKER_VOLUMES}/ssh_id_rsa:/ssh_id_rsa \
     --name docker_temp_test \
     --network work  \
     cavbot/temp_for_test bash
