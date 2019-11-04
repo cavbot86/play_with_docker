6b33af2029e566dd296e726ac6774cd2efee8d7a
hizhangrui <hizhangrui@hotmail.com>
2019-11-04 16:51:26 +0800

    rootfs/admin_install/init_common:

--- rootfs/admin_install/init_common.sh
+++ rootfs/admin_install/init_common.sh
@@ -9,11 +9,15 @@ rm -rf /tmp/*
 rm -rf /var/tmp/* 
 rm -rf /var/lib/apt/lists/* 
 
-echo "alias ll='ls -al'" >> /etc/profile 
+echo "alias ll='ls -al'" >> /etc/profile
+
 mkdir -p /init_scripts 
 mkdir -p /startup_scripts 
+
 sed -ri 's/^#?PermitRootLogin\s+.*/PermitRootLogin yes/' /etc/ssh/sshd_config 
 sed -ri 's/UsePAM yes/#UsePAM yes/g' /etc/ssh/sshd_config 
+
+# init admin user
 groupadd -g 1000 ${SUDOER_USER} 
 useradd ${SUDOER_USER} -u 1000 -s /bin/bash -g ${SUDOER_USER} 
 echo "${SUDOER_USER} ALL=(ALL) NOPASSWD: ALL " > /etc/sudoers.d/001_${SUDOER_USER}
