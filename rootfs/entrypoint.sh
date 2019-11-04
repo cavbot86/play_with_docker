4501b41e0f976bc83f64aec5ed9e92e0d8d35f03
hizhangrui <hizhangrui@hotmail.com>
2019-11-04 20:58:34 +0800

    rootfs/entrypoint:

--- rootfs/entrypoint.sh
+++ rootfs/entrypoint.sh
@@ -4,7 +4,7 @@ set -e
 if [[ ! -f "${ADMIN_RUN_DATA}/run_on_first_startup.log" ]]; then
     echo EXEC: /admin_startup/run_on_first_startup.sh
     /admin_startup/run_on_first_startup.sh
-    echo "`date`" >> ${ADMIN_RUN_DATA}/run_on_first_startup.log
+    echo "ok: `date`" >> ${ADMIN_RUN_DATA}/run_on_first_startup.log
 fi
 
 echo EXEC: /admin_startup/run_on_startup.sh
