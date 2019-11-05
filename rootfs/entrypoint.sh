247c6d85a8269ad81f60807f8e896556897fdf3c
hizhangrui <hizhangrui@hotmail.com>
2019-11-05 12:19:14 +0800

    rootfs/entrypoint:

--- rootfs/entrypoint.sh
+++ rootfs/entrypoint.sh
@@ -12,6 +12,7 @@ echo EXEC: /admin_startup/run_on_startup.sh
 
 echo start services ...
 echo "################################################################################################"
+# exec /bin/tini -- /usr/sbin/sshd -D
 /usr/sbin/sshd -D
 su - ${SUDOER_USER} -c /usr/bin/supervisord
 echo su - ${SUDOER_USER} -c "$@"
