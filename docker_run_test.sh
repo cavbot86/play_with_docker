b97d033eacf8b4212b8aec785b6294fba6511191
hizhangrui <hizhangrui@hotmail.com>
2019-11-06 15:43:40 +0800

    docker_run_test:

--- docker_run_test.sh
+++ docker_run_test.sh
@@ -11,18 +11,18 @@ sudo mkdir -p ${DOCKER_VOLUMES}/docker_temp_test/develop
 sudo mkdir -p ${DOCKER_VOLUMES}/docker_temp_test/develop/data
 sudo mkdir -p ${DOCKER_VOLUMES}/ssh_id_rsa
 
-# sudo docker run -it \
-#     -p $PORT_1:22 \
-#     -v ${DOCKER_VOLUMES}/docker_temp_test/develop/data:/data \
-#     -v ${DOCKER_VOLUMES}/ssh_id_rsa:/ssh_id_rsa \
-#     --name docker_temp_test \
-#     --network work  \
-#     cavbot/temp_for_test bash
-
-sudo docker run -d \
+sudo docker run -it \
     -p $PORT_1:22 \
     -v ${DOCKER_VOLUMES}/docker_temp_test/develop/data:/data \
     -v ${DOCKER_VOLUMES}/ssh_id_rsa:/ssh_id_rsa \
     --name docker_temp_test \
     --network work  \
-    cavbot/temp_for_test
+    cavbot/temp_for_test  "/start_app.sh & bash"
+
+# sudo docker run -d \
+#     -p $PORT_1:22 \
+#     -v ${DOCKER_VOLUMES}/docker_temp_test/develop/data:/data \
+#     -v ${DOCKER_VOLUMES}/ssh_id_rsa:/ssh_id_rsa \
+#     --name docker_temp_test \
+#     --network work  \
+#     cavbot/temp_for_test
