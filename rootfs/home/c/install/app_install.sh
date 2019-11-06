ad27e07ce70982f5bc470e43a00950b5a601d0ae
hizhangrui <hizhangrui@hotmail.com>
2019-11-06 16:28:36 +0800

    rootfs/home/c/install/app_install:

--- rootfs/home/c/install/app_install.sh
+++ rootfs/home/c/install/app_install.sh
@@ -2,7 +2,7 @@
 set -e
 ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
 apt-get update
-apt-get install git -y
+apt-get install git tzdata -y
 apt-get autoremove -y --purge
 apt-get clean
 rm -rf /tmp/*
