d9f032d4eb19a5a9674c4192034773446678867f
Cavbot <cavbot@outlook.com>
2019-11-06 08:19:56 +0800

    docker_run_test:

--- docker_run_test.sh
+++ docker_run_test.sh
@@ -8,12 +8,14 @@ fi
 
 # rm -rf ${DOCKER_VOLUMES}/docker_temp_test
 mkdir -p ${DOCKER_VOLUMES}/docker_temp_test/ubuntu-base-flat/data
+mkdir -p ${DOCKER_VOLUMES}/docker_temp_test/ubuntu-base-flat/home
 mkdir -p ${DOCKER_VOLUMES}/ssh_id_rsa
 
 
 sudo docker run -it \
     -p $PORT_1:22 \
     -v ${DOCKER_VOLUMES}/docker_temp_test/ubuntu-base-flat/data:/data \
+    -v ${DOCKER_VOLUMES}/docker_temp_test/ubuntu-base-flat/home:/home \
     -v ${DOCKER_VOLUMES}/ssh_id_rsa:/ssh_id_rsa \
     --name docker_temp_test \
     --network work  \
