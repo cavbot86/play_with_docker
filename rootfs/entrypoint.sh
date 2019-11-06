343528be7be727addb7a991f457ea404707695bb
hizhangrui <hizhangrui@hotmail.com>
2019-11-06 19:23:09 +0800

    rootfs/entrypoint:

--- rootfs/entrypoint.sh
+++ rootfs/entrypoint.sh
@@ -14,4 +14,21 @@ echo start services ...
 echo "################################################################################################"
 # exec /bin/tini -- /usr/sbin/sshd -D
 /usr/sbin/sshd -D &
-su ${SUDOER_USER} -c /usr/bin/supervisord
+if [[ ${CMD_USER} == "root" ]]; then
+    if [[ -n "$@" ]]; then
+        /usr/bin/supervisord &
+        echo "$@"
+        $@
+    else
+        /usr/bin/supervisord
+    fi
+else
+    if [[ -n "$@" ]]; then
+        su ${SUDOER_USER} -c /usr/bin/supervisord &
+        echo su ${CMD_USER} -c "$@"
+        su ${CMD_USER} -c "$@"
+    else
+        su ${SUDOER_USER} -c /usr/bin/supervisord
+    fi
+
+fi
