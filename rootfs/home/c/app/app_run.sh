5dff851b49070f1591f0a823c9a29fcf04205d65
hizhangrui <hizhangrui@hotmail.com>
2019-11-06 15:14:32 +0800

    rootfs/home/c/app/app_run:

--- rootfs/home/c/app/app_run.sh
+++ rootfs/home/c/app/app_run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -e
-mkdir -p ${WORDSPACE}/log
-echo "Hello world!" > ${WORDSPACE}/log/test.log
-tail -f ${WORDSPACE}/log/test.log
+env
+mkdir -p ${WORKSPACE}/log
+echo "Hello world!" > ${WORKSPACE}/log/test.log
+tail -f ${WORKSPACE}/log/test.log
